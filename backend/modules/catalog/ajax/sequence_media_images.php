748a88e3674bc6f95af2409743cba48631127cc5
Jesse Dobbelaere <jesse@dobbelaere-ae.be>
2014-06-30 18:20:54 +0200

    Fixes media images sequence

---
+++ backend/modules/catalog/ajax/sequence_media_images.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Alters the sequence of media images
+ *
+ * @author Jesse Dobbelaere <jesse@dobbelaere-ae.be>
+ */
+class BackendCatalogAjaxSequenceMediaImages extends BackendBaseAJAXAction
+{
+	/**
+	 * Execute the action
+	 */
+	public function execute()
+	{
+		// call parent, this will probably add some general CSS/JS or other required files
+		parent::execute();
+
+		// get parameters
+		$newIdSequence = trim(SpoonFilter::getPostValue('new_id_sequence', null, '', 'string'));
+
+		// list id
+		$ids = (array) explode(',', rtrim($newIdSequence, ','));
+
+		// loop id's and set new sequence
+		foreach($ids as $i => $id)
+		{
+			// build item
+			$item['id'] = (int) $id;
+
+			// change sequence
+			$item['sequence'] = $i + 1;
+
+			// update sequence
+			if(BackendCatalogModel::existsImage($item['id'])) BackendCatalogModel::updateImage($item);
+		}
+
+		// success output
+		$this->output(self::OK, null, 'sequence updated');
+	}
+}
