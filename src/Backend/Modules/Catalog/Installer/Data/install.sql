9f16af39452bbab7ddeb248635529cf951dea3f7
Waldo Cosman <waldo_cosman@hotmail.com>
2015-01-12 15:22:43 +0100

    Update for category-widget

--- src/Backend/Modules/Catalog/Installer/Data/install.sql
+++ src/Backend/Modules/Catalog/Installer/Data/install.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `catalog_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `meta_id` int(11) NOT NULL,
+  `extra_id` int(11) NOT NULL,
   `language` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
